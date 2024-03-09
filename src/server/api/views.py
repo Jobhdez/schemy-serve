0bc7ade0f34d7dd67f4a241a8f367ba649c5c6a2
Job Hernandez <hj93@protonmail.com>
2024-03-09 14:49:07 -0800

    add imports, setup

--- src/server/api/views.py
+++ src/server/api/views.py
@@ -1,1 +1,17 @@
-
+# compiler and interpreter api
+from src.compilers.linear_algebra_interpreter.parser import parser
+from src.compilers.linear_algebra_interpreter.interpreter import evaluate
+from src.compilers.linear_algebra_to_c.parser import parser as cparser
+from src.compilers.linear_algebra_to_c.ast_to_lalg import ast_to_lalg
+from src.compilers.linear_algebra_to_c.lalg_to_c import lalg_to_c
+from src.compilers.scm_interpreter.parser import parser as scmparser
+from src.compilers.scm_interpreter.interp import interp
+# djangorestframework api
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
+# models
+#...
+# forms
+#...
