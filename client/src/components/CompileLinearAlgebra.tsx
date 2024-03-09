aea1c24061c2354d98a40f544a7eadec6afc564d
Job Hernandez <hj93@protonmail.com>
2024-03-08 22:53:03 -0800

    add components

---
+++ client/src/components/CompileLinearAlgebra.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+
+function CompileLinearAlgebra() {
+    return (
+	<Box
+	sx={{
+	    width: 500,
+	    maxWidth: '100%',
+	}}
+	    >
+	    
