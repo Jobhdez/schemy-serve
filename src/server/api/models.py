d99553198a0f8e9e8731e1ad44d22c5e95051ab2
Job Hernandez <hj93@protonmail.com>
2024-03-09 18:16:54 -0800

    add tables

--- src/server/api/models.py
+++ src/server/api/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    lalg_interp_exps = models.ManyToManyField("LinearAlgebraInterpreter")
+    lalg_compile_exps = models.ManyToManyField("LinearAlgebraCompiler")
+    scm_interp_exps = models.ManyToManyField("SchemeInterpreter")
+    
+class Compiler(models.Model):
+
+    input_expression = models.CharField(max_length=200)
+    output_expression = models.CharField(max_length=200)
+    price = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['created']
+
+class LinearAlgebraInterpreter(Compiler):
+
+    def __str__(self):
+        return f'Linear Algebra Interpreter with input {self.input_expression} and evaluated code {self.output_expression}'
+
+class LinearAlgebraCompiler(Compiler):
+
+    def __str__(self):
+        return f'Linear Algebra Compiler with input {self.input_expression} and generated C code {self.output_expression}'
+
+class SchemeInterpreter(Compiler):
+
+    def __str__(self):
+        return f'The Scheme expression {self.input_expression} is evaluated to {self.output_expression}'
