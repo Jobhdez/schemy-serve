965d57bfd5a1cd565d33ea114f6859a262ccfb00
Job Hernandez <hj93@protonmail.com>
2024-03-09 19:52:41 -0800

    add urls

---
+++ src/server/api/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from api import views
+
+app_name = 'api'
+
+urlpatterns = [
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
+    path('lalg_interp_exps/', views.linear_algebra_interpreter, name='lalg_exps'),
+    path('lalg_compile_exps/', views.linear_algebra_compiler, name='lalg_comp_exps'),
+    path('scm_exps/', views.scheme_interpreter, name='scm_exps'),
+    ]
