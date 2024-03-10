263a20efdfe4bc002a18868b4cde39d3c68dbf31
Job Hernandez <hj93@protonmail.com>
2024-03-10 14:22:12 -0700

    update urls

--- src/server/api/urls.py
+++ src/server/api/urls.py
@@ -6,7 +6,7 @@ app_name = 'api'
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
-    path('lalg_interp_exps/', views.linear_algebra_interpreter, name='lalg_exps'),
-    path('lalg_compile_exps/', views.linear_algebra_compiler, name='lalg_comp_exps'),
+    #path('lalg_interp_exps/', views.linear_algebra_interp, name='lalg_exps'),
+    #path('lalg_compile_exps/', views.linear_algebra_compiler, name='lalg_comp_exps'),
     path('scm_exps/', views.scheme_interpreter, name='scm_exps'),
     ]
