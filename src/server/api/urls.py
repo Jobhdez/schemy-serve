085b455f3f249ae28ad8c5eb8da6d7920f01d9ba
Job Hernandez <hj93@protonmail.com>
2024-03-11 00:19:39 -0700

    add new endpoints

--- src/server/api/urls.py
+++ src/server/api/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     #path('lalg_interp_exps/', views.linear_algebra_interp, name='lalg_exps'),
     #path('lalg_compile_exps/', views.linear_algebra_compiler, name='lalg_comp_exps'),
     path('scm_exps/', views.scheme_interpreter, name='scm_exps'),
+    path('scm_uploads/', views.evaluate_scm_from_file, name='upload_scm'),
+    path('scm_expressions/', views.scheme_expressions, name='scm_exps'),        path('scm_exp/<int:id>/', views.scheme_expression, name='scm_exp'),
     ]
