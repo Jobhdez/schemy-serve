b18c1c4cfbe86085ff1a2003e52d4055f28a03ea
Job Hernandez <hj93@protonmail.com>
2024-06-22 14:59:54 -0700

    add challenge model

--- src/server/api/models.py
+++ src/server/api/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
+from django.contrib.postgres.fields import JSONField
 class User(AbstractUser):
     scm_interp_exps = models.ManyToManyField("SchemeInterpreter")
-    
+    challenges = models.ManyToManyField("Challenges")
     class Meta:
         app_label = 'api'
         
@@ -21,3 +21,15 @@ class SchemeInterpreter(Compiler):
 
     def __str__(self):
         return f'The Scheme expression {self.input_expression} is evaluated to {self.output_expression}'
+
+class Challenges(models.Model):
+  problem_statements = models.TextField()
+  solution = models.TextField()
+  created = models.DateTimeField(auto_now_add=True)
+
+  class Meta:
+    ordering = ['created']
+
+  def __str__(self):
+    return f'The problem statement: {self.problem_statement} has the corresponding solution: {self.solution}'
+
