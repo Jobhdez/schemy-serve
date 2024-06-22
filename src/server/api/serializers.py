a17b708648363c989dc4555ca46a6bdb3bfe0031
Job Hernandez <hj93@protonmail.com>
2024-06-22 15:00:28 -0700

    add challenge serializer

--- src/server/api/serializers.py
+++ src/server/api/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import SchemeInterpreter
+from .models import SchemeInterpreter, Challenges 
 
 class SchemeInterpSerializer(serializers.ModelSerializer):
     class Meta:
         model = SchemeInterpreter
         fields = ['input_expression', 'output_expression', 'created']
+
+class ChallengesSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Challenges
+    fields = ['problem_statement', 'solution']
