4a23d3ec4e17cd5e537738d4b2cf8e44d1c2ce70
Job Hernandez <hj93@protonmail.com>
2024-06-22 18:13:38 -0700

    add client functions

--- src/client/client.py
+++ src/client/client.py
@@ -2,7 +2,19 @@ import requests
 
 url = " http://127.0.0.1:8000/api/"
 
-def create_challenge(endpoint, username, password, data):
-  re = request.post(url+endpoint, data=data, auth=(username, password))
+def create_challenge(url, endpoint, username, password, data):
+  re = requests.post(url+endpoint, data=data, auth=(username, password))
   return re.json()
   
+
+def register(url, password, username, first_name, email):
+    link = url + 'register/'
+    data = {'password': password, 'password2': password, 'username':username, 'first_name': first_name, 'email': email}
+    re = requests.post(link, data=data)
+    return re.json()
+
+  
+def challenges(url):
+  re = requests.get(url)
+  return re.json()
+
