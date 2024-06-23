93bdfcbc9902803de1bfcb8b6a7ccfc70ea7c3b3
Job Hernandez <hj93@protonmail.com>
2024-06-23 15:46:57 -0700

    update challenges serializer

--- src/server/api/serializers.py
+++ src/server/api/serializers.py
@@ -1,12 +1,25 @@
 from rest_framework import serializers
-from .models import SchemeInterpreter, Challenges 
+from .models import SchemeInterpreter, Challenges, ProblemStatement, Solution
 
 class SchemeInterpSerializer(serializers.ModelSerializer):
     class Meta:
         model = SchemeInterpreter
         fields = ['input_expression', 'output_expression', 'created']
 
+class ProblemStatementSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProblemStatement
+        fields = ['problem_statement', 'created']
+
+class SolutionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Solution
+        fields = ['solution', 'created']
+
 class ChallengesSerializer(serializers.ModelSerializer):
-  class Meta:
-    model = Challenges
-    fields = ['problem_statement', 'solution', 'created']
+    problem_statement = ProblemStatementSerializer(many=True, read_only=True)
+    solution = SolutionSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Challenges
+        fields = ['problem_statement', 'solution', 'created']
