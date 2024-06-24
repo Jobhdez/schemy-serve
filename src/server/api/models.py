6017a1a6cdd59dd18cb6daeade2fe572fbfc972c
Job Hernandez <hj93@protonmail.com>
2024-06-24 09:43:04 -0700

    add app model

--- src/server/api/models.py
+++ src/server/api/models.py
@@ -6,9 +6,19 @@ class User(AbstractUser):
     challenges = models.ManyToManyField("Challenges")
     class Meta:
         app_label = 'api'
-        
-class Compiler(models.Model):
 
+class SchemeApp(models.Model):
+    users = models.ManyToManyField("User", related_name="scheme_apps")
+    created = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="owned_apps")
+
+    class Meta:
+        ordering = ['created']
+
+    def __str__(self):
+        return f'This app is owned by: {self.owner.username}'
+  
+class Compiler(models.Model):
     input_expression = models.CharField(max_length=200)
     output_expression = models.CharField(max_length=200)
     created = models.DateTimeField(auto_now_add=True)
@@ -17,37 +27,37 @@ class Compiler(models.Model):
         ordering = ['created']
 
 class SchemeInterpreter(Compiler):
-
     def __str__(self):
         return f'The Scheme expression {self.input_expression} is evaluated to {self.output_expression}'
 
 class Challenges(models.Model):
-  problem_statement = models.ManyToManyField("ProblemStatement")
-  solution = models.ManyToManyField("Solution")
-  created = models.DateTimeField(auto_now_add=True)
-  name = models.CharField(max_length=200)
-  class Meta:
-    ordering = ['created']
+    problem_statement = models.ManyToManyField("ProblemStatement")
+    solution = models.ManyToManyField("Solution")
+    created = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=200)
     
-  def __str__(self):
-    return f'The problem statement: {self.name}'
+    class Meta:
+        ordering = ['created']
+    
+    def __str__(self):
+        return f'The problem statement: {self.name}'
 
 class ProblemStatement(models.Model):
-  problem_statement = models.TextField()
-  created = models.DateTimeField(auto_now_add=True)
-  class Meta:
-    ordering=['created']
+    problem_statement = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ['created']
 
-  def __str__(self):
-    return f'the problem statement: {self.problem_statement}'
-
+    def __str__(self):
+        return f'the problem statement: {self.problem_statement}'
 
 class Solution(models.Model):
-  solution = models.TextField()
-  created = models.DateTimeField(auto_now_add=True)
-  class Meta:
-    ordering=['created']
+    solution = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ['created']
 
-  def __str__(self):
-    return f'the solution: {self.problem_statement}'
-
+    def __str__(self):
+        return f'the solution: {self.solution}'
